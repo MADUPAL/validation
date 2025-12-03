37424045467518f45d9f5692e303cf9fd6c75f1a
MADUPAL <kevin488@naver.com>
2025-12-03 10:17:44 +0900

    상세페이지와 수정

---
+++ src/main/java/com/validation/domain/post/postComment/PostComment.java
@@ -0,0 +1,23 @@
+package com.validation.domain.post.postComment;
+
+import com.validation.domain.post.post.entity.Post;
+import com.validation.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class PostComment extends BaseEntity {
+
+    @ManyToOne
+    private Post post;
+    private String content;
+
+    public PostComment(Post post, String content) {
+        this.post = post;
+        this.content = content;
+    }
+}
